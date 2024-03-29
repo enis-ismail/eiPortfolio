4fb03f91584556be062366f4c55a23111ead3721
Enis Ismail <enis89ismail@gmail.com>
2024-03-29 09:18:40 +0200

    Remove GA tracking

--- src/portfolio.js
+++ src/portfolio.js
@@ -3,8 +3,7 @@
 // Website related settings
 const settings = {
   isSplash: false, // Change this to true if you want to use the splash screen.
-  useCustomCursor: true, // Change this to false if you want the good'ol cursor
-  googleTrackingID: "UA-174238252-2",
+  useCustomCursor: false, // Change this to false if you want the good'ol cursor
 };
 
 //Home Page
